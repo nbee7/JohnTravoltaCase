b163a73474f4ca020e24132c5b4f203d9c4af5ab
nbee7 <daffadit0721@gmail.com>
2021-09-06 20:46:43 +0700

    ubah print

--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -5,15 +5,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        JohnTravolta johnTravolta = new JohnTravolta();
         System.out.print("Masukan Waktu Kerja Anda Berapa Jam Dalam Seminggu:");
         int waktu = in.nextInt();
+        int gaji = johnTravolta.hitungGaji(waktu);
+        System.out.println("Gaji anda minggu ini: "+gaji);
+
         System.out.print("Masukan Pengeluaran Anda Dalam Seminggu:");
         int pengeluaran = in.nextInt();
-
-        JohnTravolta johnTravolta = new JohnTravolta();
-        int gaji = johnTravolta.hitungGaji(waktu);
-        System.out.print("Gaji anda minggu ini: ");
-        System.out.println(gaji);
         System.out.println(johnTravolta.hitungPengeluaran(gaji,pengeluaran));
     }
 }
